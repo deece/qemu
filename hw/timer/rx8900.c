b9bb23625f85dd796f6c0e70cc5afd7affe75d4c
Alastair D'Silva <alastair@d-silva.org>
2016-11-01 14:16:44 +1100

    Implement temperature property for the emulated RX8900

--- hw/timer/rx8900.c
+++ hw/timer/rx8900.c
@@ -3,103 +3,41 @@
  *
  * Copyright (c) 2016 IBM Corporation
  * Authors:
- * 	 Alastair D'Silva <alastair@d-silva.org>
+ *  Alastair D'Silva <alastair@d-silva.org>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ * This code is licensed under the GPL version 2 or later.  See
+ * the COPYING file in the top-level directory.
  *
  * Datasheet available at:
- * 	https://support.epson.biz/td/api/doc_check.php?dl=app_RX8900CE&lang=en
+ *  https://support.epson.biz/td/api/doc_check.php?dl=app_RX8900CE&lang=en
  *
  * Not implemented:
- * 	Implement Alarm functions
- * 	Implement Timer Counters
+ *  Implement Alarm functions
+ *  Implement Timer Counters
+ *  Implement i2c timeout
  */
 
 #include "qemu/osdep.h"
 #include "qemu-common.h"
 #include "hw/i2c/i2c.h"
+#include "hw/timer/rx8900_regs.h"
 #include "qemu/bcd.h"
 #include "qemu/error-report.h"
+#include "qapi/error.h"
+#include "qapi/visitor.h"
 
 #define TYPE_RX8900 "rx8900"
 #define RX8900(obj) OBJECT_CHECK(RX8900State, (obj), TYPE_RX8900)
 
-#define NVRAM_SIZE 0x20
-
-typedef enum RX8900Addresses {
-    SECONDS = 0x00,
-    MINUTES = 0x01,
-    HOURS = 0x02,
-    WEEKDAY = 0x03,
-    DAY = 0x04,
-    MONTH = 0x05,
-    YEAR = 0x06,
-    RAM = 0x07,
-    ALARM_MINUTE = 0x08,
-    ALARM_HOUR = 0x09,
-    ALARM_WEEK_DAY = 0x0A,
-    TIMER_COUNTER_0 = 0x0B,
-    TIMER_COUNTER_1 = 0x0C,
-    EXTENSION_REGISTER = 0x0D,
-    FLAG_REGISTER = 0X0E,
-    CONTROL_REGISTER = 0X0F,
-    EXT_SECONDS = 0x010, /* Alias of SECONDS */
-    EXT_MINUTES = 0x11, /* Alias of MINUTES */
-    EXT_HOURS = 0x12, /* Alias of HOURS */
-    EXT_WEEKDAY = 0x13, /* Alias of WEEKDAY */
-    EXT_DAY = 0x14, /* Alias of DAY */
-    EXT_MONTH = 0x15, /* Alias of MONTH */
-    EXT_YEAR = 0x16, /* Alias of YEAR */
-    TEMPERATURE = 0x17,
-    BACKUP_FUNCTION = 0x18,
-    NO_USE_1 = 0x19,
-    NO_USE_2 = 0x1A,
-    EXT_TIMER_COUNTER_0 = 0x1B, /* Alias of TIMER_COUNTER_0 */
-    EXT_TIMER_COUNTER_1 = 0x1C, /* Alias of TIMER_COUNTER_1 */
-    EXT_EXTENSION_REGISTER = 0x1D, /* Alias of EXTENSION_REGISTER */
-    EXT_FLAG_REGISTER = 0X1E, /* Alias of FLAG_REGISTER */
-    EXT_CONTROL_REGISTER = 0X1F /* Alias of CONTROL_REGISTER */
-} RX8900Addresses;
-
-typedef enum ExtRegBits {
-    EXT_REG_TSEL0 = 0,
-    EXT_REG_TSEL1 = 1,
-    EXT_REG_FSEL0 = 2,
-    EXT_REG_FSEL1 = 3,
-    EXT_REG_TE = 4,
-    EXT_REG_USEL = 5,
-    EXT_REG_WADA = 6,
-    EXT_REG_TEST = 7
-} ExtRegBits;
-
-typedef enum CtrlRegBits {
-    CTRL_REG_RESET = 0,
-    CTRL_REG_WP0 = 1,
-    CTRL_REG_WP1 = 2,
-    CTRL_REG_AIE = 3,
-    CTRL_REG_TIE = 4,
-    CTRL_REG_UIE = 5,
-    CTRL_REG_CSEL0 = 6,
-    CTRL_REG_CSEL1 = 7
-} CtrlRegBits;
+static bool log;
 
 typedef struct RX8900State {
     I2CSlave parent_obj;
 
     int64_t offset;
+    uint8_t weekday; /* Saved for deferred offset calculation, 0-6 */
     uint8_t wday_offset;
-    uint8_t nvram[NVRAM_SIZE];
+    uint8_t nvram[RX8900_NVRAM_SIZE];
     int32_t ptr;
     bool addr_byte;
 } RX8900State;
@@ -108,16 +46,57 @@ static const VMStateDescription vmstate_rx8900 = {
     .name = "rx8900",
     .version_id = 2,
     .minimum_version_id = 1,
-    .fields = (VMStateField[] ) {
+    .fields = (VMStateField[]) {
         VMSTATE_I2C_SLAVE(parent_obj, RX8900State),
         VMSTATE_INT64(offset, RX8900State),
+        VMSTATE_UINT8_V(weekday, RX8900State, 2),
         VMSTATE_UINT8_V(wday_offset, RX8900State, 2),
-        VMSTATE_UINT8_ARRAY(nvram, RX8900State, NVRAM_SIZE),
+        VMSTATE_UINT8_ARRAY(nvram, RX8900State, RX8900_NVRAM_SIZE),
         VMSTATE_INT32(ptr, RX8900State),
         VMSTATE_BOOL(addr_byte, RX8900State),
         VMSTATE_END_OF_LIST()
     }
 };
+
+#define RX8900_TRACE_BUF_SIZE 256
+/**
+ * Emit a trace message
+ * @param file the source filename
+ * @param line the line number the message was emitted from
+ * @param dev the RX8900 device
+ * @param fmt a printf style format
+ */
+static void trace(const char *file, int line, const char *func,
+        I2CSlave *dev, const char *fmt, ...)
+{
+    va_list ap;
+    char buf[RX8900_TRACE_BUF_SIZE];
+    int len;
+
+    len = snprintf(buf, sizeof(buf), "\n\t%s:%s:%d: RX8900 %s %s@0x%x: %s",
+            file, func, line, dev->qdev.id, dev->qdev.parent_bus->name,
+            dev->address, fmt);
+    if (len >= RX8900_TRACE_BUF_SIZE) {
+        error_report("%s:%d: Trace buffer overflow", file, line);
+    }
+
+    va_start(ap, fmt);
+    error_vreport(buf, ap);
+    va_end(ap);
+}
+
+/**
+ * Emit a trace message
+ * @param dev the RX8900 device
+ * @param fmt a printf format
+ */
+#define TRACE(dev, fmt, ...) \
+    do { \
+        if (log) { \
+            trace(__FILE__, __LINE__, __func__, &dev, fmt, ## __VA_ARGS__); \
+        } \
+    } while (0)
+
 
 static void capture_current_time(RX8900State *s)
 {
@@ -130,7 +109,7 @@ static void capture_current_time(RX8900State *s)
     s->nvram[MINUTES] = to_bcd(now.tm_min);
     s->nvram[HOURS] = to_bcd(now.tm_hour);
 
-    s->nvram[WEEKDAY] = 0x01 << ((now.tm_wday + 1) % 7);
+    s->nvram[WEEKDAY] = 0x01 << ((now.tm_wday + s->wday_offset) % 7);
     s->nvram[DAY] = to_bcd(now.tm_mday);
     s->nvram[MONTH] = to_bcd(now.tm_mon + 1);
     s->nvram[YEAR] = to_bcd(now.tm_year % 100);
@@ -142,14 +121,25 @@ static void capture_current_time(RX8900State *s)
     s->nvram[EXT_DAY] = s->nvram[DAY];
     s->nvram[EXT_MONTH] = s->nvram[MONTH];
     s->nvram[EXT_YEAR] = s->nvram[YEAR];
+
+    TRACE(s->parent_obj, "Update current time to %02d:%02d:%02d %d %d/%d/%d "
+            "(0x%02x%02x%02x%02x%02x%02x%02x)",
+            now.tm_hour, now.tm_min, now.tm_sec,
+            (now.tm_wday + s->wday_offset) % 7,
+            now.tm_mday, now.tm_mon, now.tm_year + 1900,
+            s->nvram[HOURS], s->nvram[MINUTES], s->nvram[SECONDS],
+            s->nvram[DAY], s->nvram[MONTH], s->nvram[YEAR]);
 }
 
 static void inc_regptr(RX8900State *s)
 {
     /* The register pointer wraps around after 0x1F
      */
-    s->ptr = (s->ptr + 1) & (NVRAM_SIZE - 1);
+    s->ptr = (s->ptr + 1) & (RX8900_NVRAM_SIZE - 1);
+    TRACE(s->parent_obj, "Operating on register 0x%02x", s->ptr);
+
     if (s->ptr == 0x00) {
+        TRACE(s->parent_obj, "Register pointer has overflowed, wrapping to 0");
         capture_current_time(s);
     }
 }
@@ -160,16 +150,29 @@ static void rx8900_event(I2CSlave *i2c, enum i2c_event event)
 
     switch (event) {
     case I2C_START_RECV:
-        /* In h/w, capture happens on any START condition, not just a
-         * START_RECV, but there is no need to actually capture on
-         * START_SEND, because the guest can't get at that data
-         * without going through a START_RECV which would overwrite it.
+        /* In h/w, time capture happens on any START condition, not just a
+         * START_RECV. For the emulation, it doesn't actually matter,
+         * since a START_RECV has to occur before the data can be read.
          */
         capture_current_time(s);
         break;
     case I2C_START_SEND:
         s->addr_byte = true;
         break;
+    case I2C_FINISH:
+        if (s->weekday < 7) {
+            struct tm now;
+            qemu_get_timedate(&now, s->offset);
+
+            s->wday_offset = (s->weekday - now.tm_wday + 7) % 7;
+
+            TRACE(s->parent_obj, "Set weekday to 0x%02x, wday_offset=%d",
+                    s->weekday, s->wday_offset);
+
+            s->weekday = 7;
+        }
+        break;
+
     default:
         break;
     }
@@ -179,6 +182,7 @@ static int rx8900_recv(I2CSlave *i2c)
 {
     RX8900State *s = RX8900(i2c);
     uint8_t res = s->nvram[s->ptr];
+    TRACE(s->parent_obj, "Read register 0x%x = 0x%x", s->ptr, res);
     inc_regptr(s);
     return res;
 }
@@ -261,17 +265,23 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     RX8900State *s = RX8900(i2c);
     struct tm now;
 
+    TRACE(s->parent_obj, "Received I2C data 0x%02x", data);
+
     if (s->addr_byte) {
-        s->ptr = data & (NVRAM_SIZE - 1);
+        s->ptr = data & (RX8900_NVRAM_SIZE - 1);
+        TRACE(s->parent_obj, "Operating on register 0x%02x", s->ptr);
         s->addr_byte = false;
         return 0;
     }
+
+    TRACE(s->parent_obj, "Set data 0x%02x=0x%02x", s->ptr, data);
 
     qemu_get_timedate(&now, s->offset);
     switch (s->ptr) {
     case SECONDS:
     case EXT_SECONDS:
         now.tm_sec = from_bcd(data & 0x7f);
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[SECONDS] = data;
         s->nvram[EXT_SECONDS] = data;
         break;
@@ -279,6 +289,7 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case MINUTES:
     case EXT_MINUTES:
         now.tm_min = from_bcd(data & 0x7f);
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[MINUTES] = data;
         s->nvram[EXT_MINUTES] = data;
         break;
@@ -286,44 +297,32 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case HOURS:
     case EXT_HOURS:
         now.tm_hour = from_bcd(data & 0x3f);
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[HOURS] = data;
         s->nvram[EXT_HOURS] = data;
         break;
 
     case WEEKDAY:
     case EXT_WEEKDAY: {
-        int user_wday = 0;
+        int user_wday = ctz32(data);
         /* The day field is supposed to contain a value in
          * the range 0-6. Otherwise behavior is undefined.
          */
         switch (data) {
         case 0x01:
-            user_wday = 0;
-            break;
         case 0x02:
-            user_wday = 1;
-            break;
         case 0x04:
-            user_wday = 2;
-            break;
         case 0x08:
-            user_wday = 3;
-            break;
         case 0x10:
-            user_wday = 4;
-            break;
         case 0x20:
-            user_wday = 5;
-            break;
         case 0x40:
-            user_wday = 6;
             break;
         default:
             error_report("WARNING: RX8900 - weekday data '%x' is out of range,"
                     " undefined behavior will result", data);
             break;
         }
-        s->wday_offset = (user_wday - now.tm_wday + 7) % 7;
+        s->weekday = user_wday;
         s->nvram[WEEKDAY] = data;
         s->nvram[EXT_WEEKDAY] = data;
         break;
@@ -332,6 +331,7 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case DAY:
     case EXT_DAY:
         now.tm_mday = from_bcd(data & 0x3f);
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[DAY] = data;
         s->nvram[EXT_DAY] = data;
         break;
@@ -339,6 +339,7 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case MONTH:
     case EXT_MONTH:
         now.tm_mon = from_bcd(data & 0x1f) - 1;
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[MONTH] = data;
         s->nvram[EXT_MONTH] = data;
         break;
@@ -346,6 +347,7 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case YEAR:
     case EXT_YEAR:
         now.tm_year = from_bcd(data) + 100;
+        s->offset = qemu_timedate_diff(&now);
         s->nvram[YEAR] = data;
         s->nvram[EXT_YEAR] = data;
         break;
@@ -360,8 +362,8 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     case CONTROL_REGISTER:
     case EXT_CONTROL_REGISTER:
         validate_control_register(s, data);
-        s->nvram[EXTENSION_REGISTER] = data;
-        s->nvram[EXT_EXTENSION_REGISTER] = data;
+        s->nvram[CONTROL_REGISTER] = data;
+        s->nvram[EXT_CONTROL_REGISTER] = data;
         break;
 
     default:
@@ -372,29 +374,85 @@ static int rx8900_send(I2CSlave *i2c, uint8_t data)
     return 0;
 }
 
+/**
+ * Calculate the device temperature in Celcius
+ */
+static void rx8900_get_temperature(Object *obj, Visitor *v, const char *name,
+                                   void *opaque, Error **errp)
+{
+    RX8900State *s = RX8900(obj);
+    double value = (s->nvram[TEMPERATURE] * 2.0f - 187.1f) / 3.218f;
+
+    TRACE(s->parent_obj, "Read temperature property, 0x%x = %f°C",
+            s->nvram[TEMPERATURE], value);
+
+    visit_type_number(v, name, &value, errp);
+}
+
+/**
+ * Set the device temperature in Celcius
+ */
+static void rx8900_set_temperature(Object *obj, Visitor *v, const char *name,
+                                   void *opaque, Error **errp)
+{
+    RX8900State *s = RX8900(obj);
+    Error *local_err = NULL;
+    double temp; /* degrees Celcius */
+
+    visit_type_number(v, name, &temp, &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
+        return;
+    }
+    if (temp >= 100 || temp < -58) {
+        error_setg(errp, "value %f°C is out of range", temp);
+        return;
+    }
+
+    s->nvram[TEMPERATURE] = (uint8_t) ((temp * 3.218f + 187.19f) / 2);
+
+    TRACE(s->parent_obj, "Set temperature property, 0x%x = %f°C",
+            s->nvram[TEMPERATURE], temp);
+}
+
+
 static int rx8900_init(I2CSlave *i2c)
 {
+    TRACE(*i2c, "Initialized");
+
     return 0;
+}
+
+static void rx8900_initfn(Object *obj)
+{
+    object_property_add(obj, "temperature", "number",
+                        rx8900_get_temperature,
+                        rx8900_set_temperature, NULL, NULL, NULL);
 }
 
 static void rx8900_reset(DeviceState *dev)
 {
     RX8900State *s = RX8900(dev);
 
+    TRACE(s->parent_obj, "Reset");
+
     /* The clock is running and synchronized with the host */
     s->offset = 0;
-    memset(s->nvram, 0, NVRAM_SIZE);
+    s->weekday = 7; /* Set to an invalid value */
+    memset(s->nvram, 0, RX8900_NVRAM_SIZE);
 
     /* Temperature formulation from the datasheet
-     * ( TEMP[ 7:0 ] * 2 – 187.19) / 3.218
+     * ( TEMP[ 7:0 ] * 2 - 187.19) / 3.218
      *
-     * Hardcode it to 25 degrees Celcius
+     * Set the initial state to 25 degrees Celcius
      */
     s->nvram[TEMPERATURE] = 135; /* (25 * 3.218 + 187.19) / 2 */
 
-    s->nvram[CONTROL_REGISTER] = 1 << CTRL_REG_CSEL0;
+    s->nvram[CONTROL_REGISTER] = BIT(CTRL_REG_CSEL0);
 
     s->ptr = 0;
+    TRACE(s->parent_obj, "Operating on register 0x%02x", s->ptr);
+
     s->addr_byte = false;
 }
 
@@ -415,11 +473,13 @@ static const TypeInfo rx8900_info = {
     .name = TYPE_RX8900,
     .parent = TYPE_I2C_SLAVE,
     .instance_size = sizeof(RX8900State),
+    .instance_init = rx8900_initfn,
     .class_init = rx8900_class_init,
 };
 
 static void rx8900_register_types(void)
 {
+    log = getenv("RX8900_TRACE") != NULL;
     type_register_static(&rx8900_info);
 }
 
